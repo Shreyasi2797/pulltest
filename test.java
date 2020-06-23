3e685061ad1fee866940338e04fb677a1f3c8fe2
Shreyasi2797 <54217980+Shreyasi2797@users.noreply.github.com>
2020-06-23 23:33:29 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class test{
+public static void main(String args[]){
+System.out.println("Hello world");
+Scanner s = new Scanner(System.in);
+System.out.println("Enter first number");
+int a = s.nextInt();
+System.out.println("Enter second number");
+int b = s.nextInt();
+System.out.println("Addition: "+(a+b));
+System.out.println("Subtraction: "+(a-b));
+System.out.println("Product: "+(a*b));
+System.out.println("Division: "+(a/b));
+}
+}
